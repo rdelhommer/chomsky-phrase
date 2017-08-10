38fc34c51fe33a6d2f63c35edfce5c29619a0475
Rick Delhommer <rdelhommer@gmail.com>
2017-08-10 14:44:23 -0700

    add default factory

--- bin/chomsky-phrase.js
+++ bin/chomsky-phrase.js
@@ -1,44 +1,8 @@
 #!/usr/bin/env node
  
 var chomsky = require('../index.js');
-var resultFilters = require('../lib/result-filters');
 
-var phraseFactory = new chomsky.PhraseFactory();
-
-var nounConfig = new chomsky.WordnikConfig('noun');
-var nounFactory = new chomsky.WordFactory(nounConfig, [
-  resultFilters.filterWithEndings([ 's' ]),
-  resultFilters.filterProperNouns,
-  resultFilters.filterContractions
-]);
-
-var verbConfig = new chomsky.WordnikConfig('verb');
-var verbFactory = new chomsky.WordFactory(verbConfig, [
-  resultFilters.filterWithOutEndings([ 'ing', 'ed', 's', 'en' ]),
-  resultFilters.filterContractions
-]);
-
-var adjectiveConfig = new chomsky.WordnikConfig('adjective');
-var adjectiveFactory = new chomsky.WordFactory(adjectiveConfig, [
-  resultFilters.filterWithEndings([ 'y', 'ing' ]),
-  resultFilters.filterContractions
-]);
-
-var adverbConfig = new chomsky.WordnikConfig('adverb');
-var adverbFactory = new chomsky.WordFactory(adverbConfig, [
-  resultFilters.filterWithEndings([ 'ly' ]),
-  resultFilters.filterContractions
-]);
-
-var numberFactory = new chomsky.NumberFactory(1, 100);
-
-var phraseFactory = new chomsky.PhraseFactory([
-  numberFactory,
-  adjectiveFactory,
-  nounFactory,
-  verbFactory,
-  adverbFactory
-]);
+var phraseFactory = chomsky.PhraseFactory.createDefault();
 
 phraseFactory.create((phrase) => {
   console.log(phrase);
