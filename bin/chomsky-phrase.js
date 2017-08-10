6219b96587d3276df6170f23ff1cb8a4a66188f2
Rick Delhommer <rdelhommer@gmail.com>
2017-08-10 14:34:32 -0700

    make configurable

--- bin/chomsky-phrase.js
+++ bin/chomsky-phrase.js
@@ -1,9 +1,45 @@
 #!/usr/bin/env node
  
-var lib = require('../index.js');
-var phraseFactory = new lib.PhraseFactory();
+var chomsky = require('../index.js');
+var resultFilters = require('../lib/result-filters');
 
-phraseFactory.getPhrase()
-  .then((phraseArray) => {
-    console.log(phraseArray);
-  });
+var phraseFactory = new chomsky.PhraseFactory();
+
+var nounConfig = new chomsky.WordnikConfig('noun');
+var nounFactory = new chomsky.WordFactory(nounConfig, [
+  resultFilters.filterWithEndings([ 's' ]),
+  resultFilters.filterProperNouns,
+  resultFilters.filterContractions
+]);
+
+var verbConfig = new chomsky.WordnikConfig('verb');
+var verbFactory = new chomsky.WordFactory(verbConfig, [
+  resultFilters.filterWithOutEndings([ 'ing', 'ed', 's', 'en' ]),
+  resultFilters.filterContractions
+]);
+
+var adjectiveConfig = new chomsky.WordnikConfig('adjective');
+var adjectiveFactory = new chomsky.WordFactory(adjectiveConfig, [
+  resultFilters.filterWithEndings([ 'y', 'ing' ]),
+  resultFilters.filterContractions
+]);
+
+var adverbConfig = new chomsky.WordnikConfig('adverb');
+var adverbFactory = new chomsky.WordFactory(adverbConfig, [
+  resultFilters.filterWithEndings([ 'ly' ]),
+  resultFilters.filterContractions
+]);
+
+var numberFactory = new chomsky.NumberFactory(1, 100);
+
+var phraseFactory = new chomsky.PhraseFactory([
+  numberFactory,
+  adjectiveFactory,
+  nounFactory,
+  verbFactory,
+  adverbFactory
+]);
+
+phraseFactory.create((phrase) => {
+  console.log(phrase);
+});
--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 var PhraseFactory = require('./lib/phrase-factory');
+var WordFactory = require('./lib/word-factory');
+var NumberFactory = require('./lib/number-factory');
+var WordnikConfig = require('./lib/wordnik-config');
 
 module.exports = {
-  PhraseFactory: PhraseFactory
+  PhraseFactory: PhraseFactory,
+  WordFactory: WordFactory,
+  NumberFactory: NumberFactory,
+  WordnikConfig: WordnikConfig
 };
