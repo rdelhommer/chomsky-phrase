7ed047f2cb3644aba156c1be18627b3633938347
Rick Delhommer <rdelhommer@gmail.com>
2017-08-25 14:34:51 -0700

    add default result filters to index

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ var PhraseFactory = require('./lib/phrase-factory');
 var WordFactory = require('./lib/word-factory');
 var NumberFactory = require('./lib/number-factory');
 var WordnikConfig = require('./lib/wordnik-config');
+var resultFilters = require('./lib/result-filters');
 
 module.exports = {
   PhraseFactory: PhraseFactory,
   WordFactory: WordFactory,
   NumberFactory: NumberFactory,
-  WordnikConfig: WordnikConfig
+  WordnikConfig: WordnikConfig,
+  resultFilters: resultFilters
 };
